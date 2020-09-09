81764777d8762c0d625d9d32782f665150529244
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-09-09 16:55:09 -0300

    Update
    Interface inicial e um pouco de responsividade

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+  stagePadding: 50,
+  loop:false,
+  margin:10,
+  dots:false,
+  nav:false,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
